7e2425400499c6af839a6525c66b0153069d6338
shayan heidary <hdrydeveloper@gmail.com>
2023-10-24 13:23:54 +0200

    feedback

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./../page.module.css";
+import { useState, FormEvent, ChangeEvent } from "react";
+import FeedBack from "../components/Feedback";
+export default function FeedBackPage() {
+  return (
+    <>
+      <FeedBack />
+    </>
+  );
+}
